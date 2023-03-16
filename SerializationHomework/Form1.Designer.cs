1fc73aa547b535d7857a1b014e4ec0bf03424822
Acer <Acer@DESKTOP-FSEDNBO>
2023-03-16 01:17:14 -0700

    Add project files.

---
+++ SerializationHomework/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace SerializationHomework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEmpolyeeId = new System.Windows.Forms.Label();
+            this.lblEmpolyeeName = new System.Windows.Forms.Label();
+            this.lblLocation = new System.Windows.Forms.Label();
+            this.txtEmpolyeeId = new System.Windows.Forms.TextBox();
+            this.txtEmpolyeeName = new System.Windows.Forms.TextBox();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.btnBinaryWrite = new System.Windows.Forms.Button();
+            this.btnXmlWrite = new System.Windows.Forms.Button();
+            this.btnSoapWrite = new System.Windows.Forms.Button();
+            this.btnJsonWrite = new System.Windows.Forms.Button();
+            this.btnBinaryRead = new System.Windows.Forms.Button();
+            this.btnXmlRead = new System.Windows.Forms.Button();
+            this.btnSoapRead = new System.Windows.Forms.Button();
+            this.btnJsonRead = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblEmpolyeeId
+            // 
+            this.lblEmpolyeeId.AutoSize = true;
+            this.lblEmpolyeeId.Location = new System.Drawing.Point(105, 37);
+            this.lblEmpolyeeId.Name = "lblEmpolyeeId";
+            this.lblEmpolyeeId.Size = new System.Drawing.Size(62, 13);
+            this.lblEmpolyeeId.TabIndex = 0;
+            this.lblEmpolyeeId.Text = "EmployeeId";
+            // 
+            // lblEmpolyeeName
+            // 
+            this.lblEmpolyeeName.AutoSize = true;
+            this.lblEmpolyeeName.Location = new System.Drawing.Point(105, 88);
+            this.lblEmpolyeeName.Name = "lblEmpolyeeName";
+            this.lblEmpolyeeName.Size = new System.Drawing.Size(81, 13);
+            this.lblEmpolyeeName.TabIndex = 1;
+            this.lblEmpolyeeName.Text = "EmpolyeeName";
+            // 
+            // lblLocation
+            // 
+            this.lblLocation.AutoSize = true;
+            this.lblLocation.Location = new System.Drawing.Point(105, 135);
+            this.lblLocation.Name = "lblLocation";
+            this.lblLocation.Size = new System.Drawing.Size(48, 13);
+            this.lblLocation.TabIndex = 2;
+            this.lblLocation.Text = "Location";
+            // 
+            // txtEmpolyeeId
+            // 
+            this.txtEmpolyeeId.Location = new System.Drawing.Point(207, 37);
+            this.txtEmpolyeeId.Name = "txtEmpolyeeId";
+            this.txtEmpolyeeId.Size = new System.Drawing.Size(134, 20);
+            this.txtEmpolyeeId.TabIndex = 3;
+            // 
+            // txtEmpolyeeName
+            // 
+            this.txtEmpolyeeName.Location = new System.Drawing.Point(207, 88);
+            this.txtEmpolyeeName.Name = "txtEmpolyeeName";
+            this.txtEmpolyeeName.Size = new System.Drawing.Size(134, 20);
+            this.txtEmpolyeeName.TabIndex = 4;
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.Location = new System.Drawing.Point(207, 128);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.Size = new System.Drawing.Size(133, 20);
+            this.txtLocation.TabIndex = 5;
+            // 
+            // btnBinaryWrite
+            // 
+            this.btnBinaryWrite.Location = new System.Drawing.Point(65, 188);
+            this.btnBinaryWrite.Name = "btnBinaryWrite";
+            this.btnBinaryWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnBinaryWrite.TabIndex = 6;
+            this.btnBinaryWrite.Text = "Binary Write";
+            this.btnBinaryWrite.UseVisualStyleBackColor = true;
+            this.btnBinaryWrite.Click += new System.EventHandler(this.btnBinaryWrite_Click);
+            // 
+            // btnXmlWrite
+            // 
+            this.btnXmlWrite.Location = new System.Drawing.Point(65, 226);
+            this.btnXmlWrite.Name = "btnXmlWrite";
+            this.btnXmlWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnXmlWrite.TabIndex = 7;
+            this.btnXmlWrite.Text = "XML Write";
+            this.btnXmlWrite.UseVisualStyleBackColor = true;
+            this.btnXmlWrite.Click += new System.EventHandler(this.btnXmlWrite_Click);
+            // 
+            // btnSoapWrite
+            // 
+            this.btnSoapWrite.Location = new System.Drawing.Point(65, 264);
+            this.btnSoapWrite.Name = "btnSoapWrite";
+            this.btnSoapWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnSoapWrite.TabIndex = 8;
+            this.btnSoapWrite.Text = "SOAP Write";
+            this.btnSoapWrite.UseVisualStyleBackColor = true;
+            this.btnSoapWrite.Click += new System.EventHandler(this.btnSoapWrite_Click);
+            // 
+            // btnJsonWrite
+            // 
+            this.btnJsonWrite.Location = new System.Drawing.Point(65, 304);
+            this.btnJsonWrite.Name = "btnJsonWrite";
+            this.btnJsonWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnJsonWrite.TabIndex = 9;
+            this.btnJsonWrite.Text = "JSON Write";
+            this.btnJsonWrite.UseVisualStyleBackColor = true;
+            this.btnJsonWrite.Click += new System.EventHandler(this.btnJsonWrite_Click);
+            // 
+            // btnBinaryRead
+            // 
+            this.btnBinaryRead.Location = new System.Drawing.Point(265, 188);
+            this.btnBinaryRead.Name = "btnBinaryRead";
+            this.btnBinaryRead.Size = new System.Drawing.Size(75, 23);
+            this.btnBinaryRead.TabIndex = 10;
+            this.btnBinaryRead.Text = "Binary Read";
+            this.btnBinaryRead.UseVisualStyleBackColor = true;
+            this.btnBinaryRead.Click += new System.EventHandler(this.btnBinaryRead_Click);
+            // 
+            // btnXmlRead
+            // 
+            this.btnXmlRead.Location = new System.Drawing.Point(265, 226);
+            this.btnXmlRead.Name = "btnXmlRead";
+            this.btnXmlRead.Size = new System.Drawing.Size(75, 23);
+            this.btnXmlRead.TabIndex = 11;
+            this.btnXmlRead.Text = "XML Read";
+            this.btnXmlRead.UseVisualStyleBackColor = true;
+            this.btnXmlRead.Click += new System.EventHandler(this.btnXmlRead_Click);
+            // 
+            // btnSoapRead
+            // 
+            this.btnSoapRead.Location = new System.Drawing.Point(266, 264);
+            this.btnSoapRead.Name = "btnSoapRead";
+            this.btnSoapRead.Size = new System.Drawing.Size(75, 23);
+            this.btnSoapRead.TabIndex = 12;
+            this.btnSoapRead.Text = "SOAP Read";
+            this.btnSoapRead.UseVisualStyleBackColor = true;
+            this.btnSoapRead.Click += new System.EventHandler(this.btnSoapRead_Click);
+            // 
+            // btnJsonRead
+            // 
+            this.btnJsonRead.Location = new System.Drawing.Point(265, 304);
+            this.btnJsonRead.Name = "btnJsonRead";
+            this.btnJsonRead.Size = new System.Drawing.Size(75, 23);
+            this.btnJsonRead.TabIndex = 13;
+            this.btnJsonRead.Text = "JSON Read";
+            this.btnJsonRead.UseVisualStyleBackColor = true;
+            this.btnJsonRead.Click += new System.EventHandler(this.btnJsonRead_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnJsonRead);
+            this.Controls.Add(this.btnSoapRead);
+            this.Controls.Add(this.btnXmlRead);
+            this.Controls.Add(this.btnBinaryRead);
+            this.Controls.Add(this.btnJsonWrite);
+            this.Controls.Add(this.btnSoapWrite);
+            this.Controls.Add(this.btnXmlWrite);
+            this.Controls.Add(this.btnBinaryWrite);
+            this.Controls.Add(this.txtLocation);
+            this.Controls.Add(this.txtEmpolyeeName);
+            this.Controls.Add(this.txtEmpolyeeId);
+            this.Controls.Add(this.lblLocation);
+            this.Controls.Add(this.lblEmpolyeeName);
+            this.Controls.Add(this.lblEmpolyeeId);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEmpolyeeId;
+        private System.Windows.Forms.Label lblEmpolyeeName;
+        private System.Windows.Forms.Label lblLocation;
+        private System.Windows.Forms.TextBox txtEmpolyeeId;
+        private System.Windows.Forms.TextBox txtEmpolyeeName;
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.Button btnBinaryWrite;
+        private System.Windows.Forms.Button btnXmlWrite;
+        private System.Windows.Forms.Button btnSoapWrite;
+        private System.Windows.Forms.Button btnJsonWrite;
+        private System.Windows.Forms.Button btnBinaryRead;
+        private System.Windows.Forms.Button btnXmlRead;
+        private System.Windows.Forms.Button btnSoapRead;
+        private System.Windows.Forms.Button btnJsonRead;
+    }
+}
+
---
+++ SerializationHomework/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;  //Binary serialization
+using System.Runtime.Serialization.Formatters.Soap;  //SOAP serialization
+using System.Xml.Serialization;                        //XML serialization
+using System.Text.Json;                                //JSON serialization
+using System.Windows.Forms;
+using System.Linq.Expressions;
+
+namespace SerializationHomework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBinaryWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileStream fs = new FileStream(@"C:\DotNet20DecBatch\empBinary.dat", FileMode.Create, FileAccess.Write);
+                Empolyee emp = new Empolyee();
+                emp.Id = Convert.ToInt32(txtEmpolyeeId.Text);
+                emp.Name = txtEmpolyeeName.Text;
+                emp.Location = txtLocation.Text;
+                BinaryFormatter binaryFormatter = new BinaryFormatter();
+                binaryFormatter.Serialize(fs, emp);
+                MessageBox.Show("Data Saved");
+                fs.Close();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnBinaryRead_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                FileStream fs = new FileStream(@"C:\DotNet20DecBatch\empBinary.dat", FileMode.Open, FileAccess.Read);
+                Empolyee emp = new Empolyee();
+                BinaryFormatter binaryFormatter = new BinaryFormatter();
+                emp = (Empolyee)binaryFormatter.Deserialize(fs);
+                txtEmpolyeeId.Text = emp.Id.ToString();
+                txtEmpolyeeName.Text = emp.Name;
+                txtLocation.Text = emp.Location;
+                fs.Close();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnXmlWrite_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                FileStream fs = new FileStream(@"C:\DotNet20DecBatch\empxml.xml", FileMode.Create, FileAccess.Write);
+                Empolyee emp = new Empolyee();
+                emp.Id = Convert.ToInt32(txtEmpolyeeId.Text);
+                emp.Name = txtEmpolyeeName.Text;
+                emp.Location = txtLocation.Text;
+                XmlSerializer xmlserializer = new XmlSerializer(typeof(Empolyee));
+                xmlserializer.Serialize(fs, emp);
+                MessageBox.Show("Data Saved");
+                fs.Close();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnXmlRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileStream fs = new FileStream(@"C:\DotNet20DecBatch\empxml.xml", FileMode.Open, FileAccess.Read);
+                Empolyee emp = new Empolyee();
+                XmlSerializer xmlserializer = new XmlSerializer(typeof(Empolyee));
+                emp = (Empolyee)xmlserializer.Deserialize(fs);
+                txtEmpolyeeId.Text = emp.Id.ToString();
+                txtEmpolyeeName.Text = emp.Name;
+                txtLocation.Text = emp.Location;
+                fs.Close();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSoapWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileStream fs = new FileStream(@"C:\DotNet20DecBatch\empSoap.soap", FileMode.Create, FileAccess.Write);
+                Empolyee emp = new Empolyee();
+                emp.Id = Convert.ToInt32(txtEmpolyeeId.Text);
+                emp.Name = txtEmpolyeeName.Text;
+                emp.Location = txtLocation.Text;
+                SoapFormatter soapFormatter = new SoapFormatter();
+                soapFormatter.Serialize(fs, emp);
+                MessageBox.Show("Data Saved");
+                fs.Close();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSoapRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileStream fs = new FileStream(@"C:\DotNet20DecBatch\empSoap.soap", FileMode.Open, FileAccess.Read);
+                Empolyee emp = new Empolyee();
+                SoapFormatter soapFormatter = new SoapFormatter();
+                emp = (Empolyee)soapFormatter.Deserialize(fs);
+                txtEmpolyeeId.Text = emp.Id.ToString();
+                txtEmpolyeeName.Text = emp.Name;
+                txtLocation.Text = emp.Location;
+                fs.Close();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnJsonWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileStream fs = new FileStream(@"C:\DotNet20DecBatch\deptJson.json", FileMode.Create, FileAccess.Write);
+                Empolyee emp = new Empolyee();
+                emp.Id = Convert.ToInt32(txtEmpolyeeId.Text);
+                emp.Name = txtEmpolyeeName.Text;
+                emp.Location = txtLocation.Text;
+
+                JsonSerializer.Serialize<Empolyee>(fs, emp);
+                MessageBox.Show("Data Saved");
+                fs.Close();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnJsonRead_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                FileStream fs = new FileStream(@"D:\DotNet20DecBatch\deptJson.json", FileMode.Open, FileAccess.Read);
+                Empolyee emp = new Empolyee();
+
+                emp = JsonSerializer.Deserialize<Empolyee>(fs);
+                txtEmpolyeeId.Text = emp.Id.ToString();
+                txtEmpolyeeName.Text = emp.Name;
+                txtLocation.Text = emp.Location;
+                fs.Close();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
